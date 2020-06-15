b68fe02b2daa0e8f35fef0381c9f69895565a75d
Azrielle <azriellepelayo@gmail.com>
2020-06-15 17:59:40 +0800

    Created the render function to add the created DOM elements in the page Added id = "trash" and id = "check" to the check circle and trash icon because set attribute overwrites the class name

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Get HTML elements
 
 // For displaying books
-
 let read = document.querySelector(".check")
 let title = document.querySelector(".title");
 let author = document.querySelector(".author");
@@ -34,7 +33,6 @@ function Book(title, author, pages, status){
 
 // Create a function to add the book to the library
 
-
 addBookToLibrary();
 function addBookToLibrary(){
     showHideForm();
@@ -45,11 +43,11 @@ function addBookToLibrary(){
         let newBook = new Book(newTitle.value, newAuthor.value, newPages.value, checkStatus);
 
         library.push(newBook);
-        console.log(library[0]);
+        console.log(library);
         clearForm();
+        render();
 
     })
-    
 }
 
 function showHideForm(){
@@ -69,7 +67,47 @@ function clearForm(){
     newPages.value = "";
     status.checked = false;
 }
+
 //Create a function that renders the data to the page
 function render(){
+    for(let element of library){
+        let container = document.createElement("div");
+        //Make the classname of container equal to the string equivalent of its index in the library array
+        container.className = library.indexOf(element).toString();
+        container.id = "data";
+        container.setAttribute("id", "data");
 
+        let checkbox = document.createElement("i");
+        checkbox.className = "fas fa-check-circle check";
+        checkbox.id = "check"
+        checkbox.setAttribute("id", "check");
+
+        let bookTitle = document.createElement("p");
+        bookTitle.className = "title";
+        bookTitle.textContent = element.title;
+        bookTitle.setAttribute("class", "title");
+
+        let bookAuthor = document.createElement("p");
+        bookAuthor.className = "author";
+        bookAuthor.textContent = element.author;
+        bookAuthor.setAttribute("class", "author")
+
+        let bookPages = document.createElement("p");
+        bookPages.className = "pages";
+        bookPages.textContent = element.pages + " pages";
+        bookPages.setAttribute("class", "pages")
+
+        let trashcan = document.createElement("i");
+        trashcan.className = "fas fa-trash";
+        trashcan.id = "trash";
+        trashcan.setAttribute("id", "trash");
+
+        // append the children
+        listContainer.appendChild(container);
+        container.appendChild(checkbox);
+        container.appendChild(bookTitle);
+        container.appendChild(bookAuthor);
+        container.appendChild(bookPages);
+        container.appendChild(trashcan);
+    }
 }
